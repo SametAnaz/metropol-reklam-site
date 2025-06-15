d63088a90584ff6fc0ff44d86d7b978f25d52a40
Samet <sametanaz.tr@gmail.com>
2025-06-15 04:47:39 +0300

    bugfix#4

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,9 @@
 import '../styles/globals.css';
 import { SessionProvider } from 'next-auth/react';
 import { DefaultSeo } from 'next-seo';
-import { Analytics } from "@vercel/analytics/react" ;
+//import { Analytics } from "@vercel/analytics/react" ;
 import { Analytics } from "@vercel/analytics/next" ;
-import { SpeedInsights } from '@vercel/speed-insights/react';
+//import { SpeedInsights } from '@vercel/speed-insights/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import SEO from '../next-seo.config';
 
